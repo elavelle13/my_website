9c77b1daf684daf9320eab7e7dabb93139611880
Emily <emily@uncubed.com>
2015-09-01 14:41:05 -0400

    updated site

--- js/app.js
+++ js/app.js
@@ -1,7 +1,66 @@
-$(document).ready(function() {
+$(document).ready(function(){
+      $(".about-container").show();
+        $('#about').toggleClass("active");
+	    	$('#media').not(this).removeClass("active");
+	           $('#web').not(this).removeClass("active");
+	              $('#resume').not(this).removeClass("active");
+	                    $( ".media-container" ).hide( "fast" );
+	                      $( ".web-container" ).hide( "fast" );
+	                        $( ".resume-container" ).hide( "fast" );
+    });
 
-  // // slide bar navigation
-  $.slidebars();
-  
+    
+   $(document).ready(function() {
+        $('#media').click(function() {
+          $('#about').not(this).removeClass("active");
+            $('#web').not(this).removeClass("active");
+              $('#resume').not(this).removeClass("active");
+                $(this).toggleClass("active");
+                  $('.media-container').slideToggle("fast");
+                    $( ".about-container" ).hide( "fast" );
+                      $( ".resume-container" ).hide( "fast" );
+                        $( ".web-container" ).hide( "fast" );
+        });
+    });
 
-});
+   $(document).ready(function() {
+        $('#about').click(function() {
+          $('#media').not(this).removeClass("active");
+            $('#web').not(this).removeClass("active");
+              $('#resume').not(this).removeClass("active");
+                $(this).toggleClass("active");
+                  $('.about-container').slideToggle("fast");
+                    $( ".media-container" ).hide( "fast" );
+                      $( ".web-container" ).hide( "fast" );
+                        $( ".resume-container" ).hide( "fast" );
+
+        });
+    });
+
+      $(document).ready(function() {
+        $('#web').click(function() {
+          $('#about').not(this).removeClass("active");
+            $('#media').not(this).removeClass("active");
+              $('#resume').not(this).removeClass("active");
+                $(this).toggleClass("active");
+                  $('.web-container').slideToggle("fast");
+                    $( ".about-container" ).hide( "fast" );
+                      $( ".media-container" ).hide( "fast" );
+                        $( ".resume-container" ).hide( "fast" );
+        });
+    });
+
+   $(document).ready(function() {
+        $('#resume').click(function() {
+          $('#about').not(this).removeClass("active");
+            $('#media').not(this).removeClass("active");
+              $('#web').not(this).removeClass("active");
+                $(this).toggleClass("active");
+                  $('.resume-container').hide("fast");
+                    $( ".about-container" ).hide( "fast" );
+                      $( ".media-container" ).hide( "fast" );
+                        $( ".web-container" ).hide( "fast" );
+        });
+
+    });
+
---
+++ js/skills-courses.js
@@ -0,0 +1,23 @@
+
+
+
+
+$(document).ready(function() {
+        $('#skills').click(function() {
+          $('#courses').not(this).removeClass("active");
+                $(this).toggleClass("active");
+                  $('.skills-container').slideToggle("fast");
+                    $( ".courses-container" ).hide( "fast" );
+                      
+        });
+    });
+
+ $(document).ready(function() {
+        $('#courses').click(function() {
+          $('#skills').not(this).removeClass("active");
+                $(this).toggleClass("active");
+                  $('.courses-container').slideToggle("fast");
+                    $( ".skills-container" ).hide( "fast" );
+                      
+        });
+    });
